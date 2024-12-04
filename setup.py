c84fdd54f784a5121bb2817eada2997ce8baef1a
MohamedEmam82 <mohamed.elahikh.emam@gmail.com>
2024-12-04 07:52:40 +0400

    install requirements.txt file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CoinMasker',
+    version= '0.0.1',
+    author= 'Mohamed Emam',
+    author_email= 'mohamed.salaheldin.emam@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
