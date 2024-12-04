4fbc47cee5668f968061b88ce1d7f1c5bf040e00
MohamedEmam82 <mohamed.elahikh.emam@gmail.com>
2024-12-04 15:42:21 +0400

    utils functuons created

---
+++ CoinMasker/utils/main_utils.py
@@ -0,0 +1,24 @@
+import os.path
+import sys
+import yaml
+import base64
+
+from CoinMasker.exception import AppException
+from CoinMasker.logger import logging
+
+
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open("./data/" + fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
+
+    
+    
