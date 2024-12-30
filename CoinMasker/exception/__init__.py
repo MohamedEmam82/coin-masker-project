028353fb3388ee5386db8b0a9afb11fc9d48f1d9
MohamedEmam82 <mohamed.elahikh.emam@gmail.com>
2024-12-30 20:45:12 +0400

    utils functions written

--- CoinMasker/exception/__init__.py
+++ CoinMasker/exception/__init__.py
@@ -14,6 +14,7 @@ def error_message_detail(error, error_detail: sys):
 
 
 class AppException(Exception):
+    
     def __init__(self, error_message, error_detail):
         """
         :param error_message: error message in string format
